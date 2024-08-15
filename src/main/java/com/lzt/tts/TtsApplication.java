16f78f47d9057812ea4230a004d062fb036c9d3e
tianyagogogo <tianyagogogo11111111@test.com>
2024-08-15 09:59:17 +0800

    提交

--- src/main/java/com/lzt/tts/TtsApplication.java
+++ src/main/java/com/lzt/tts/TtsApplication.java
@@ -2,8 +2,12 @@ package com.lzt.tts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TtsApplication {
 
 	public static void main(String[] args) {
